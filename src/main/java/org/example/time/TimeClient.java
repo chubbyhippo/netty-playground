a2fda71842f9e1e870cb9923d8561b85887510d3
ch <mkmsacc@outlook.com>
2023-03-01 12:42:38 +0700

    Reformat code

--- src/main/java/org/example/time/TimeClient.java
+++ src/main/java/org/example/time/TimeClient.java
@@ -5,7 +5,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class TimeClient {
